e24ad3afc70fe9b7e6f95829e202c71192037005
Kaium Uddin <kaium.uddin@welldev.io>
2024-06-23 12:20:00 +0600

    Create Sign in functionality for user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user), notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "users#index"
 
+  resource :session, only: [:new, :create, :destroy]
+
   resources :users
   get 'signup', to: 'users#new'
 end
