8c9ed6eb51aaf76615a60f640ce07adbf42cfca1
Kaium Uddin <kaium.uddin@welldev.io>
2024-06-27 18:29:25 +0600

    Add edit events pages and functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,4 +7,25 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to event_path(@event), notice: "Event successfully updated!"
+    else
+      flash.now[:alert] = "Error updating event."
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :price, :starts_at, :capacity, :image_file_name)
+  end
+
 end
