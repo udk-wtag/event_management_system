7b6ba65e30a75125a866982715e2e4a37c50ba05
Kaium Uddin <kaium.uddin@welldev.io>
2024-06-30 11:53:45 +0600

    Add delete functionality for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -36,6 +36,12 @@ class EventsController < ApplicationController
     end
   end
 
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_url, status: :see_other
+  end
+
   private
 
   def event_params
