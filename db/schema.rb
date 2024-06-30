fd4d196ce6570d8744522e23612cd3cab8bcfb1d
Kaium Uddin <kaium.uddin@welldev.io>
2024-06-30 18:08:20 +0600

    Create registration model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_27_052948) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_30_115810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_27_052948) do
     t.integer "capacity", default: 1
   end
 
+  create_table "registrations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_registrations_on_event_id"
+    t.index ["user_id"], name: "index_registrations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -35,4 +44,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_27_052948) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "registrations", "events"
+  add_foreign_key "registrations", "users"
 end
