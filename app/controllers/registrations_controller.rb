dc2a12a21cc4171eee8aab070b9e2cdf216dc548
Kaium Uddin <kaium.uddin@welldev.io>
2024-07-01 11:26:23 +0600

    Add create register for event registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,20 @@ class RegistrationsController < ApplicationController
     @event = Event.find(params[:event_id])
     @registrations = @event.registrations
   end
+
+  def new
+    @event = Event.find(params[:event_id])
+    @registration = @event.registrations.new
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @registration = @event.registrations.new
+    @registration.user = current_user
+    if @registration.save
+      redirect_to event_registrations_url(@event), notice: "Thanks for registering!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
