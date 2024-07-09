cf498e100c4b67ebcdc44a2b8e9bd891aa32dc18
udk-wtag <kaium.uddin@welldev.io>
2024-07-09 11:26:09 +0600

    Remove column image_file_name from event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_09_043525) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_09_052425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,7 +50,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_09_043525) do
     t.datetime "updated_at", null: false
     t.datetime "starts_at", null: false
     t.text "description", null: false
-    t.string "image_file_name", default: "placeholder.png"
     t.integer "capacity", default: 1
   end
 
