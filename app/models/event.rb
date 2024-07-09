4e78226d7bd5d278d21752154ab9d5f43ba0bd49
udk-wtag <kaium.uddin@welldev.io>
2024-07-09 13:10:36 +0600

    Remove image_file_path

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,6 @@ class Event < ApplicationRecord
   validates :location, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :capacity, numericality: { only_integer: true, greater_than: 0 }
-  validates :image_file_name, format: { with: /\w+\.(jpg|png)\z/i, message: 'must be a JPG or PNG image' }
   
 
   def self.upcoming
