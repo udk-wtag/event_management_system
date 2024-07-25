f5386b6fb6b33c1481a72ec7b11c32cbf9d9172b
udk-wtag <kaium.uddin@welldev.io>
2024-07-25 15:13:59 +0600

    Fix email to citext

--- db/migrate/20240609101057_create_users.rb
+++ db/migrate/20240609101057_create_users.rb
@@ -1,11 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
+    enable_extension 'citext' unless extension_enabled?('citext')
+    
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password_digest
+      t.string :name, null: false
+      t.citext :email, null: false
+      t.string :password_digest, null: false
 
       t.timestamps
     end
+
+    add_index :users, :email, unique: true
   end
 end
--- db/migrate/20240612102426_add_constraints_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddConstraintsToUsers < ActiveRecord::Migration[7.1]
-  def change
-    change_column_null :users, :name, false
-    change_column_null :users, :email, false
-    change_column_null :users, :password_digest, false
-
-    add_index :users, :email, unique: true
-  end
-end
