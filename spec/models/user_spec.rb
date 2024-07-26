a6b658b47fb7e8884bd89336e1ccd421ecbaf884
udk-wtag <kaium.uddin@welldev.io>
2024-07-26 14:33:51 +0600

    Use shoulda_matchers for all test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,36 +5,12 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to validate_presence_of(:password) }
     it { is_expected.to validate_length_of(:password).is_at_least(6) }
     it { is_expected.to have_secure_password }
-
-    it 'is not valid with an improperly formatted email' do
-      invalid_emails = ['user@foo,com', 'user_at_foo.org', 'example.user@foo.']
-      invalid_emails.each do |invalid_email|
-        user = build(:user, email: invalid_email)
-        expect(user).not_to be_valid
-        expect(user.errors[:email]).to include("is invalid")
-      end
-    end
-
-    it 'is valid with a properly formatted email' do
-      valid_emails = ['user@foo.COM', 'A_US-ER@foo.bar.org', 'first.last@foo.jp', 'alice+bob@baz.cn']
-      valid_emails.each do |valid_email|
-        user = build(:user, email: valid_email)
-        expect(user).to be_valid
-      end
-    end
-
-    it 'is not valid without matching password confirmation' do
-      user = build(:user, password: 'password123', password_confirmation: 'different')
-      expect(user).not_to be_valid
-      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
-    end
-
-    it 'is not valid with a short password' do
-      user = build(:user, password: 'short', password_confirmation: 'short')
-      expect(user).not_to be_valid
-      expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
-    end
+    it { is_expected.to allow_value('user@foo.com').for(:email) }
+    it { is_expected.not_to allow_value('user@foo,com').for(:email) }
+    it { is_expected.not_to allow_value('user_at_foo.org').for(:email) }
+    it { is_expected.not_to allow_value('example.user@foo.').for(:email) }
   end
 end
